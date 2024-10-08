af9c39ee20a4f67343d561abcf5ce6870f62275e
ligmaster <136215596+ligmaster3@users.noreply.github.com>
2024-10-08 12:28:11 -0500

    Update Sign up.php

--- src/view/Sign up.php
+++ src/view/Sign up.php
@@ -10,12 +10,19 @@
 
     <link rel="shortcut icon" href="/assets/img/logo/users-alt(1).png">
 
-    <link rel="stylesheet" media="screen" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
+    <link rel="stylesheet" type="text/css" href="/src/public/css/form.css">
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
 
-    <!-- Custom styles for our template -->
-    <link rel="stylesheet" href="/src/public/css/form.css">
+    <script src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
+    <script src="https://kit.fontawesome.com/ae360af17e.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+    <!-- <script src="/src/public/js/script.js" async></script> -->
 
     <!-- Article main content -->
     <article class="col-xs-12 maincontent">
