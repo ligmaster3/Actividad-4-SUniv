186465ef3c871319406d3e526d5c1bea4a888d70
ligmaster <136215596+ligmaster3@users.noreply.github.com>
2024-10-09 13:20:36 -0500

    Update dashboards.php

--- src/view/dashboards.php
+++ src/view/dashboards.php
@@ -94,24 +94,10 @@ $sql = "SELECT
 <body>
 
     <header>
-        <?php
-
-        if (isset($_SESSION['usuario'])) {
-            $nombre = $_SESSION['usuario']['nombre'];
-            $apellido = $_SESSION['usuario']['apellido'];
-            $email = $_SESSION['usuario']['email'];
-        } else {
-            $nombre = "Invitado";
-            $apellido = "";
-            $email = "No disponible";
-        }
-        ?>
-
         <nav class="navbar navbar-expand-lg bg-primary-subtle">
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">Sistema Academico</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
-                    aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarText">
@@ -120,24 +106,19 @@ $sql = "SELECT
                             <a class="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                                aria-expanded="false">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
                                 Cursos
                             </a>
-                            <hr class="dropdown-divider">
                             <ul class="dropdown-menu">
                                 <li><a class="dropdown-item" href="#">Creditos</a></li>
                             </ul>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Pricing</a>
-                        </li>
                     </ul>
 
-                    <!-- Avatar -->
+                    <!-- Avatar y detalles de usuario -->
                     <div class="nav-item dropdown">
                         <a class="btn btn-icon dropdown-toggle" id="navbarDropdownUserImage" href="#" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
+                            data-bs-toggle="dropdown">
                             <img class="rounded-circle" style="width: 40px; height: 40px;"
                                 src="/assets/img/logo/profile-1.png" alt="User Avatar">
                         </a>
@@ -146,24 +127,20 @@ $sql = "SELECT
                                 <img class="rounded-circle me-3" style="width: 50px; height: 50px;"
                                     src="/assets/img/logo/profile-1.png" alt="User Avatar">
                                 <div>
-                                    <span class="dropdown-user-details-name">
-                                        <?php echo $row['nombre'] . " " . $row['apellido']; ?>
-                                    </span><br>
-                                    <span class="dropdown-user-details-email">
-                                        <?php echo $row['correo']; ?>
-                                    </span>
+                                    <span
+                                        class="dropdown-user-details-name"><?php echo $nombre . " " . $apellido; ?></span><br>
+                                    <span class="dropdown-user-details-email"><?php echo $email; ?></span>
                                 </div>
                             </h6>
                             <div class="dropdown-divider"></div>
+                            <?php if (isset($_SESSION['user_id'])): ?>
+
                             <a class="dropdown-item d-flex pl-1" href="/src/view/perfil.php">
                                 <div class="dropdown-item-icon px-1">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
                                         fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                        stroke-linejoin="round" class="feather feather-settings">
-                                        <circle cx="12" cy="12" r="3"></circle>
-                                        <path
-                                            d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z">
-                                        </path>
+                                        stroke-linejoin="round" class="feather feather-log-out">
+                                        <path d="M16 17l5-5-5-5m6 5H3"></path>
                                     </svg>
                                 </div>
                                 <p>
@@ -175,21 +152,33 @@ $sql = "SELECT
                                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
                                         fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                                         stroke-linejoin="round" class="feather feather-log-out">
-                                        <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
-                                        <polyline points="16 17 21 12 16 7"></polyline>
-                                        <line x1="21" y1="12" x2="9" y2="12"></line>
+                                        <path d="M16 17l5-5-5-5m6 5H3"></path>
                                     </svg>
                                 </div>
                                 <p>
                                     Logout
                                 </p>
                             </a>
+                            <?php else: ?>
+                            <a class="dropdown-item d-flex pl-1" href="/src/view/login.php">
+                                <!-- ... (puedes agregar un ícono de Login) ... -->
+                                <div class="dropdown-item-icon px-1">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
+                                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                        stroke-linejoin="round" class="feather feather-log-out">
+                                        <path d="M16 17l5-5-5-5m6 5H3"></path>
+                                    </svg>
+                                </div>
+                                <p>Login</p>
+                            </a>
+                            <?php endif; ?>
                         </div>
                     </div>
                 </div>
             </div>
         </nav>
     </header>
+
 
     <!-- Código para mostrar la lista de usuarios registrados -->
     <div class="container shadow p-3 mb-5 bg-body-tertiary rounded">
