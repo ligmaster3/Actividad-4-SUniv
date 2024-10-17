fe47234d44536e90e5eb657d1b5727f504b25fdf
ligmaster <136215596+ligmaster3@users.noreply.github.com>
2024-10-16 19:25:07 -0500

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://kit.fontawesome.com/ae360af17e.js" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
-    <script src="/src/js/script.js" async></script>
+    <!-- <script src="/src/js/script.js" async></script> -->
 </head>
 <?php
 session_start();
