811fd35c622a9336d28c69d91ec8737456b5fbc1
ligmaster <136215596+ligmaster3@users.noreply.github.com>
2024-10-19 18:58:36 -0500

    Update scipt.js

--- src/js/scipt.js
+++ src/js/scipt.js
@@ -12,32 +12,6 @@ function showToast(message, type) {
         position: "center",
         backgroundColor: type === 'error' ? "#ff0000" : "#4CAF50",
     }).showToast();
-    document.getElementById('loginForm').addEventListener('submit', function(event) {
-        event.preventDefault();
-        
-        const email = document.getElementById('inputEmailAddress').value;
-        const password = document.getElementById('inputPassword').value;
-
-        if (!email || !password) {
-            showToast('Por favor, complete todos los campos', 'error');
-            return;
-        }
-
-        if (!isValidEmail(email)) {
-            showToast('Por favor, ingrese un email válido', 'error');
-            return;
-        }
-
-        // If validation passes, submit the form
-        this.submit();
-    });
-
-    function isValidEmail(email) {
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return emailRegex.test(email);
-    }
-
-   
 };
 
 
