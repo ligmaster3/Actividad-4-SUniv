8049f5edfacd7d1c95e22f59e33335a7938249dc
ligmaster <136215596+ligmaster3@users.noreply.github.com>
2024-10-20 02:20:55 -0500

    Update dashboards.php

--- src/public/dashboards.php
+++ src/public/dashboards.php
@@ -1,6 +1,6 @@
 <?php
 // Iniciar la sesión
-session_start();
+
 
 include "/Users/eniga/OneDrive/Documentos/GitHub/Actividad-4-SUniv/src/view/conexion.php";
 
@@ -100,91 +100,6 @@ $sql = "SELECT
 </head>
 
 <body>
-
-    <header>
-        <nav class="navbar navbar-expand-lg bg-primary-subtle">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Sistema Academico</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarText">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item dropdown ">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
-                                Cursos
-                            </a>
-                            <ul class="dropdown-menu bg-primary-subtle">
-                                <li><a class="dropdown-item" href="#">Creditos</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-
-                    <!-- Avatar y detalles de usuario -->
-                    <div class="nav-item dropdown">
-                        <a class="btn btn-icon dropdown-toggle" id="navbarDropdownUserImage" href="#" role="button"
-                            data-bs-toggle="dropdown">
-                            <img class="rounded-circle" style="width: 40px; height: 40px;"
-                                src="/assets/img/logo/profile-1.png" alt="User Avatar">
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-end">
-                            <h6 class="dropdown-header d-flex align-items-center">
-                                <img class="rounded-circle me-3" style="width: 50px; height: 50px;"
-                                    src="/assets/img/logo/profile-1.png" alt="User Avatar">
-                                <div>
-                                    <span
-                                        class="dropdown-user-details-name"><?php echo $nombre . " " . $apellido; ?></span><br>
-                                    <span class="dropdown-user-details-email"><?php echo $email; ?></span>
-                                </div>
-                            </h6>
-                            <div class="dropdown-divider"></div>
-                            <?php if (isset($_SESSION['user_id'])): ?>
-
-                            <a class="dropdown-item d-flex pl-1" href="/src/view/perfil.php">
-                                <div class="dropdown-item-icon px-1">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
-                                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                        stroke-linejoin="round" class="feather feather-log-out">
-                                        <path d="M16 17l5-5-5-5m6 5H3"></path>
-                                    </svg>
-                                </div>
-                                <p>
-                                    Account
-                                </p>
-                            </a>
-                            <a class="dropdown-item d-flex pl-1" href="/src/view/login.php">
-                                <div class="dropdown-item-icon px-1">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
-                                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                        stroke-linejoin="round" class="feather feather-log-out">
-                                        <path d="M16 17l5-5-5-5m6 5H3"></path>
-                                    </svg>
-                                </div>
-                                <a href="logout.php">Cerrar sesión</a>
-                            </a>
-                            <?php else: ?>
-                            <a class="dropdown-item d-flex pl-1" href="/src/view/login.php">
-                                <!-- ... (puedes agregar un ícono de Login) ... -->
-                                <div class="dropdown-item-icon px-1">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
-                                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                        stroke-linejoin="round" class="feather feather-log-out">
-                                        <path d="M16 17l5-5-5-5m6 5H3"></path>
-                                    </svg>
-                                </div>
-                                <p>Login</p>
-                            </a>
-                            <?php endif; ?>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </nav>
-    </header>
-
 
 
     <!-- Código para mostrar la lista de estudiantes registrados -->
